3d408f9d49f36b5ca69d427b8193674c779fbcf5
Hugo Venega <hugo.venega.0@gmail.com>
2021-07-17 21:18:41 -0300

    All numeric functions

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ function mostrar(){
 }
 
 function uno(){
-  x= x + "1";
+ x= x + "1";
  mostrar();
 }
+function dos(){
+ x= x + "2";
+ mostrar();
+}
+function tres(){
+ x= x + "3";
+ mostrar();
+}
+function cuatro(){
+ x= x + "4";
+ mostrar();
+}
+function cinco(){
+ x= x + "5";
+ mostrar();
+}
+function seis(){
+ x= x + "6";
+ mostrar();
+}
+function siete(){
+ x= x + "7";
+ mostrar();
+}  
+function ocho(){
+ x= x + "8";
+ mostrar();
+}
+function nueve(){
+ x= x + "9";
+ mostrar();
+}
+function cero(){
+ x= x + "0";
+ mostrar();
+}
+   
+   
